474873a4b08b473b1b629ee8a93c918766a7cb4e
srinivas-nahak <srinivas.nahak@gmail.com>
2023-10-15 20:19:46 +0530

    Changed the link

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:4000"
-    : "http://tech-trove-six.vercel.app/";
+    : "http://tech-trove-six.vercel.app";
 
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
