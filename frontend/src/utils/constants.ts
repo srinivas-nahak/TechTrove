8f769367f07723ca00f015453b1a9f199e336be9
srinivas-nahak <srinivas.nahak@gmail.com>
2023-10-15 20:15:31 +0530

    Added vercel production link

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,5 +1,7 @@
 export const BASE_URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:4000" : "";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4000"
+    : "http://tech-trove-six.vercel.app/";
 
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
